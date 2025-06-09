2c209b87454696218234756abfc2d4d56b9e89cc
Or Zellinger <or.zellinger@gmail.com>
2025-06-09 17:26:44 +0300

    Create the name menu calss

---
+++ Malshinon/Models/Menu.cs
@@ -0,0 +1,20 @@
+﻿namespace Malshinon.Models
+{
+    public class Menu
+    {
+        public static string[] EnterName()
+        {
+            string[] fullName = new string[2];
+
+            Console.WriteLine("Please enter your full name (two words)");
+            string[] userPrompt = Console.ReadLine()!.Split();
+            if (userPrompt.Length != 2)
+            {
+                throw new Exception("Invalid Name");
+            }
+            fullName[0] = userPrompt[0];
+            fullName[1] = userPrompt[1];
+            return fullName;
+        }
+    }
+}
