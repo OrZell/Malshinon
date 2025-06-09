9bf5ec37690353097865153b2df684fce7141c9b
Or Zellinger <or.zellinger@gmail.com>
2025-06-09 17:18:58 +0300

    Craete Proces class

--- Malshinon/Models/ProcessText.cs
+++ Malshinon/Models/ProcessText.cs
@@ -1,1 +1,22 @@
-﻿
+﻿namespace Malshinon.Models
+{
+    public class ProcessText
+    {
+        public static List<string> ProcessTextFromReporter(string Text)
+        {
+            List<string> finalResult = new List<string>();
+            List<string> TextInWords = Text.Split().ToList();
+
+            for (int i = 0; i < TextInWords.Count-1; i++)
+            {
+                if (TextInWords[i] == TextInWords[i].ToUpper() && TextInWords[i+1] == TextInWords[i+1].ToUpper())
+                {
+                    finalResult.Add(TextInWords[i]);
+                    finalResult.Add(TextInWords[i+1]);
+                    finalResult.Add(Text);
+                }
+            }
+            return finalResult;
+        }
+    }
+}
