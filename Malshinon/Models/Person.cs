26a2f63f76e6c17e8c970493f42778d0b4298405
Or Zellinger <or.zellinger@gmail.com>
2025-06-09 15:21:30 +0300

    Create the Person class

---
+++ Malshinon/Models/Person.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon.Models
+{
+    public class Person
+    {
+        private string? _FirstName;
+        public string FirstName
+        {
+            get { return _FirstName!; }
+            set { _FirstName = value; }
+        }
+
+        private string? _LastName;
+        public string LastName
+        {
+            get { return _LastName!; }
+            set { _LastName = value; }
+        }
+
+        private string? _SecretCode;
+        public string SecretCode
+        {
+            get { return _SecretCode!; }
+            set { _SecretCode = value; }
+        }
+
+        private int _Type;
+        public int Type
+        {
+            get { return _Type; }
+            set
+            {
+                try
+                {
+                    _Type = value;
+                }
+                catch (FormatException ex)
+                {
+                    throw new FormatException(ex.Message);
+                }
+            }
+        }
+
+        private int _NumReports;
+        public int NumReports
+        {
+            get { return _NumReports; }
+            set
+            {
+                try
+                {
+                    _NumReports = value;
+                }
+                catch (FormatException ex)
+                {
+                    throw new FormatException(ex.Message);
+                }
+            }
+        }
+
+        private int _NumMentions;
+        public int NumMentions
+        {
+            get { return _NumMentions; }
+            set
+            {
+                try
+                {
+                _NumMentions = value;
+                }
+                catch (FormatException ex)
+                {
+                    throw new FormatException(ex.Message);
+                }
+            }
+        }
+
+        public Person(string firstName, string lastName, )
+        {
+
+        }
+    }
+}
